e948dfbe7972809d38e90c17031aaca3bc0634ac
Falcao1303 <lucasjesus22@hotmail.com>
2023-05-31 22:00:45 -0300

    estruturando o back-end

--- ecommerce-angular+node/index.js
+++ ecommerce-angular+node/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const route = require('./routes/routes')
 
 
 // Iniciar o servidor
---
+++ ecommerce-angular+node/routes/routes.js
@@ -0,0 +1,79 @@
+const route = require('express').Router()
+const Produto = require('../controllers/produto')
+
+
+    route.get('/produto/',async  (req,res) =>{
+     const results = await tableModelProducts.listar();
+     res.status(200)
+     const serializeProduct = new ProductSerialize(
+        res.getHeader('Content-Type')
+    )
+    res.send(
+        serializeProduct.serialize(results) 
+    )         
+    })
+    
+    route.post('/',async(req,res,next)=>{
+        try{
+            const dados = req.body
+            const produto = new Produto(dados)
+             await produto.criar()
+             res.status(201)
+             const serializeProduct = new ProductSerialize(
+                res.getHeader('Content-Type')
+            )
+             res.send(
+                serializeProduct.serialize(produto) 
+             )      
+        }catch(erro){
+            next(erro)
+        }
+
+    })
+
+    route.get('/produto/:idProduto',async(req,res,next)=>{
+        try{
+            const id = req.params.idProduto
+            const produto = new Produto({ id : id})
+            await produto.carregar()
+            res.status(200)
+            const serializeProduct = new ProductSerialize(
+                res.getHeader('Content-Type')
+            )
+            res.send(
+                serializeProduct.serialize(produto) 
+            )
+        }catch(erro){
+            next(erro)
+        }
+    })
+
+    route.put('/produto/:idProduto',async(req,res,next)=>{
+        try{
+            const id = req.params.idProduto
+            const dadosRecebidos = req.body
+            const dados = Object.assign({},dadosRecebidos,{id : id})
+            const produto = new Produto(dados)
+            await produto.atualizar()
+            res.status(204)
+            res.end()
+        }catch(erro){
+            next(erro)
+        }
+    })
+
+    route.delete('/produto/:idProduto',async(req,res,next)=>{
+        try{
+            const id = req.params.idProduto
+            const produto = new Produto({ id : id})
+            await produto.carregar()
+            await produto.remover()
+            res.status(204)
+            res.end()
+        }catch(erro){
+            next(erro)
+        }
+    })
+
+module.exports = route
+
