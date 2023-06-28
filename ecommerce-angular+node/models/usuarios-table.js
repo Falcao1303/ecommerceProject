ddd0214bde0d853f110db301e2883852f0235a4e
Falcao1303 <lucasjesus22@hotmail.com>
2023-06-28 20:51:42 -0300

    ajustando get todos usuários

--- ecommerce-angular+node/models/usuarios-table.js
+++ ecommerce-angular+node/models/usuarios-table.js
@@ -3,20 +3,23 @@ const conexao = require ('./connection.js');
 
 
 
-const  Usuarios = {
+
+const Usuarios = conexao.define('usuarios', {
     id: {
         type: Sequelize.INTEGER,
-        autoIncrement : true,
-        allowNull : false,
+        autoIncrement: true,
+        allowNull: false,
         primaryKey: true
     },
     nome: {
         type: Sequelize.STRING,
-        allowNull : false
+        allowNull: false
     },
     email: Sequelize.STRING,
     senha: Sequelize.STRING
-}
+}, {
+    timestamps: false
+});
 
-module.exports = conexao.define('usuarios',Usuarios);
+module.exports = Usuarios;
  
