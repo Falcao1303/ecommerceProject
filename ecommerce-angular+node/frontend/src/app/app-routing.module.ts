935e0143ee1f8ed51cbf9ea90ac60fdb7f85d2f8
Falcao1303 <lucasjesus22@hotmail.com>
2023-11-21 22:42:28 -0300

    ajustando front end

--- ecommerce-angular+node/frontend/src/app/app-routing.module.ts
+++ ecommerce-angular+node/frontend/src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { TelaCadastroComponent } from './tela-cadastro/tela-cadastro.component';
 
 const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent
+  },
+  {
+    path: 'tela_cadastro',
+    component: TelaCadastroComponent
   },
 ];
 
