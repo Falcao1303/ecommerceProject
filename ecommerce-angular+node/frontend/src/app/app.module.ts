375a2273d3dfd01b24058f17f435423abb48b30d
Falcao1303 <lucasjesus22@hotmail.com>
2024-08-14 21:26:31 -0300

    ajustando máscara

--- ecommerce-angular+node/frontend/src/app/app.module.ts
+++ ecommerce-angular+node/frontend/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { NgxMaskDirective, NgxMaskPipe, provideNgxMask } from 'ngx-mask';
+import { NgxMaskModule } from 'ngx-mask';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -19,9 +19,10 @@ import { TelaCadastroComponent } from './components/tela-cadastro/tela-cadastro.
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    NgxMaskModule.forRoot()
   ],
-  providers: [ provideNgxMask( ) ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
